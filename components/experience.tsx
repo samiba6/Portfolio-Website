6e99020e4142c7ca2e58555171b7de78c3853852
Sami <s.babouche@esi-sba.dz>
2024-08-17 19:31:12 +0200

    fix

--- components/experience.tsx
+++ components/experience.tsx
@@ -2,14 +2,15 @@
 
 import React from "react";
 import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
+
 import "react-vertical-timeline-component/style.min.css";
 import { experiencesData } from "@/lib/data";
 import { useSectionInView } from "@/lib/hooks";
 import { useTheme } from "@/context/theme-context";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 
 export default function Experience() {
   const { ref } = useSectionInView("Experience");
